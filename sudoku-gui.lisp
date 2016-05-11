67068eddeb67e0f8a03bc731b25b913be3e886af
matthewschallenkamp <matthew.schallenkamp@gmail.com>
2016-05-11 17:04:11 -0500

    reorganized

--- sudoku-gui.lisp
+++ sudoku-gui.lisp
@@ -13,37 +13,13 @@
 (defmacro add-draw (funct)
   `(setf draws (cons ,funct draws)))
 
-(defmacro s-incf (item)
-  `(progn
-     (incf ,item)
-     (when (> ,item 9) (setf ,item 0))
-     ,item))
-
-(defmacro sudoku-button ((ix iy l h num) &body body)
+(defmacro add-button ((ix iy l h text) &body body)
   `(let ((bcolor *white*) (scolor *black*) (tcolor *black*))
     (add-draw (lambda () 
-                (let ((nnum ,num))
-                (new-draw-string-solid-* ;(write-to-string ,text)
-                  (if (= 0 nnum) "" (write-to-string nnum))
-                  (+ 13 ,ix) (+ 6 ,iy) :color tcolor :font *default-font*))))
+                (new-draw-string-solid-*
+                  ,text (+ 13 ,ix) (+ 6 ,iy) :justify :center :color tcolor :font *default-font*)))
     (add-draw (lambda () (draw-box-* ,ix ,iy ,l ,h :color bcolor :stroke-color scolor)))
     (setf button-events (cons (lambda (button state x y) ,@body) button-events))))
-
-(defun square (x y)
-  (let ((sx (- x (mod x 3))) (sy (- y (mod y 3))))
-    (loop for ox from 0 upto 2
-          appending (loop for oy from 0 upto 2 collect (list (+ sx ox) (+ sy oy))))))
-
-
-(defun possibles (board x y)
-  (let ((possible '(1 2 3 4 5 6 7 8 9)))
-    (loop for oy from 0 upto 8
-          do (setf possible (delete (aref board x oy) possible)))
-    (loop for ox from 0 upto 8
-          do (setf possible (delete (aref board ox y) possible)))
-    (loop for (ox oy) in (square x y)
-          do (setf possible (delete (aref board ox oy) possible)))
-    possible))
 
 (defun run-sudoku ()
   ;basic data
@@ -59,14 +35,14 @@
       ;add the buttons for each spot
       (loop for ix from 1 upto 9
         do (loop for iy from 1 upto 9
-          do (let ((sx (* ix *bsize*)) (sy (* iy *bsize*)) (tx (1- ix)) (ty (1- iy)) (count 0))
-              (sudoku-button (sx sy *bsize* *bsize* count)
+          do (let ((sx (* ix *bsize*)) (sy (* iy *bsize*)) (tx (1- ix)) (ty (1- iy)) (spot 0))
+              (add-button (sx sy *bsize* *bsize* (if (= 0 spot) "" (write-to-string spot)))
                 (when (and (and (> x sx) (< x (+ sx *bsize*)))
                          (and (> y sy) (< y (+ sy *bsize*))))
                   ;(setf bcolor (sdl:color :r (random 255) :g (random 255) :b (random 255)))
-                  (s-incf count)
+                  (s-incf spot)
                   ;later pull out to get next-possible
-                  (setf (aref board tx ty) count))))))
+                  (setf (aref board tx ty) spot))))))
       
    		(sdl:with-events ()
         (:quit-event () t)
--- sudoku-solve.lisp
+++ sudoku-solve.lisp
@@ -1,1 +1,25 @@
 (in-package :sudoku-play)
+
+(defmacro s-incf (item)
+  `(progn
+     (incf ,item)
+     (when (> ,item 9) (setf ,item 0))
+     ,item))
+
+
+(defun square (x y)
+  (let ((sx (- x (mod x 3))) (sy (- y (mod y 3))))
+    (loop for ox from 0 upto 2
+          appending (loop for oy from 0 upto 2 collect (list (+ sx ox) (+ sy oy))))))
+
+
+(defun possibles (board x y)
+  (let ((possible '(1 2 3 4 5 6 7 8 9)))
+    (loop for oy from 0 upto 8
+          do (setf possible (delete (aref board x oy) possible)))
+    (loop for ox from 0 upto 8
+          do (setf possible (delete (aref board ox y) possible)))
+    (loop for (ox oy) in (square x y)
+          do (setf possible (delete (aref board ox oy) possible)))
+    possible))
+
